632784e5c1953cc70dd44598b4f0980cda87cddb
Narendra Khadayat <132155378+NarendraKhadayat@users.noreply.github.com>
2025-08-16 02:12:39 +0530

    Add files via upload

---
+++ api/counter.js
@@ -0,0 +1,36 @@
+
+/**
+ * @author Narendra Khadayat
+ */
+//
+
+
+import { kv } from '@vercel/kv';
+
+
+export const config = {
+  runtime: 'edge',
+};
+
+
+export default async function handler(request) {
+  try {
+    const newCount = await kv.incr('visitorCount');
+
+    return new Response(
+      JSON.stringify({ count: newCount }),
+      {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: error.message }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+}
