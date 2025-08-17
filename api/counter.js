708918611b0d7bddcc7b84aaa26428e3d5fa9147
Narendra Khadayat <132155378+NarendraKhadayat@users.noreply.github.com>
2025-08-17 17:29:50 +0530

    Update counter.js

--- api/counter.js
+++ api/counter.js
@@ -5,32 +5,33 @@
 //
 
 
-import { kv } from '@vercel/kv';
+// import { kv } from '@vercel/kv';
 
 
-export const config = {
-  runtime: 'edge',
-};
+// export const config = {
+//   runtime: 'edge',
+// };
 
 
-export default async function handler(request) {
-  try {
-    const newCount = await kv.incr('visitorCount');
+// export default async function handler(request) {
+//   try {
+//     const newCount = await kv.incr('visitorCount');
 
-    return new Response(
-      JSON.stringify({ count: newCount }),
-      {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' },
-      }
-    );
-  } catch (error) {
-    return new Response(
-      JSON.stringify({ error: error.message }),
-      {
-        status: 500,
-        headers: { 'Content-Type': 'application/json' },
-      }
-    );
-  }
+//     return new Response(
+//       JSON.stringify({ count: newCount }),
+//       {
+//         status: 200,
+//         headers: { 'Content-Type': 'application/json' },
+//       }
+//     );
+//   } catch (error) {
+//     return new Response(
+//       JSON.stringify({ error: error.message }),
+//       {
+//         status: 500,
+//         headers: { 'Content-Type': 'application/json' },
+//       }
+//     );
+//   }
 }
+
